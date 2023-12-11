0641594a9a5e75d3a2779a9dcbb1ee422eee7acc
simoncmh2005 <112465978+simoncmh2005@users.noreply.github.com>
2023-12-11 10:13:54 +0800

    Update 88.MergeSortedArray.java

--- 88.MergeSortedArray.java
+++ 88.MergeSortedArray.java
@@ -29,13 +29,9 @@ import java.util.Arrays;
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
-
         Arrays.sort(nums1);
-
-        for (int i = 0; i < nums1.length; i++) {
-            System.out.println(nums1[i]);
-        }
     }
 }
+
 
 */
